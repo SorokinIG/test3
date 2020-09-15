1239ccacc3d1cadb5f150240d1ea3194731e58c6
nellepost5 <68441532+nellepost5@users.noreply.github.com>
2020-09-15 16:21:35 +0300

    *
    *

---
+++ WpfApp1/WpfApp1/ApplicationViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+
+namespace WpfApp1
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+       
+
+
+        private Department selectedDepartment;
+        private Personal selectedPersonal;
+
+
+    
+        public ObservableCollection<Department> Departments { get; set; }
+        public ObservableCollection<Personal> Personals { get; set; }
+
+        public Department SelectedDepartment
+        {
+            get { return selectedDepartment; }
+            set
+            {
+                selectedDepartment = value;
+                OnPropertyChanged("SelectedDepartment");
+            }
+        }
+        
+           public Personal SelectedPersonal
+        {
+               get { return selectedPersonal; }
+               set
+               {
+                   selectedPersonal = value;
+                   OnPropertyChanged("SelectedPersonal");
+               }
+           }
+          
+    
+        public ApplicationViewModel()
+        {
+
+            Departments = new ObservableCollection<Department>( Department.GetDepartments());
+
+            Personals = new ObservableCollection<Personal>(Personal.GetPersonals());
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/Department.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+namespace WpfApp1
+{
+   public class Department : INotifyPropertyChanged
+    {
+        private string name;
+
+       public int ID { get; set; }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public static Department[] GetDepartments()
+        {
+            var result = new[]
+            {
+                new Department() {ID = 1,  Name="Первый отдел"  },
+                new Department() {ID = 2,  Name="Второй отдел"  },
+                new Department() {ID = 3,  Name="Третий отдел"  },
+                new Department() {ID = 4,  Name="Четвертый отдел"  }
+
+            };
+            return result;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+    }
+}
+
